add8d23ddd18d9e2aae07152a43de9a750fc3438
Azhar Mehmood <azhark3113@gmail.com>
2022-09-14 17:56:38 +0500

    Complete-Clone

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,8 +1,6 @@
-import Image from "next/image"
-
 function Avatar({url,className}) {
   return (
-    <Image 
+    <image 
     className={`h-8 rounded-full cursor-pointer transition duration-150 transform hover:scale-110 ${className}`}
     loading="lazy"
     src={url}
