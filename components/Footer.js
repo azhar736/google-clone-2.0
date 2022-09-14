1451e6939e8fe463328b860f2e0f3eb078472c0e
Azhar Mehmood <azhark3113@gmail.com>
2022-09-14 16:24:50 +0500

    adding env file

--- components/Footer.js
+++ components/Footer.js
@@ -10,17 +10,12 @@ function Footer() {
        Made with ❤️ by Azhar Mehmood for Educational Purposes.
        </div>
        <div className="flex justify-center space-x-8 whitespace-nowrap md:justify-self-start">
-        {/* <p>Advertising</p> */}
         <a href="https://ads.google.com/">Advertising</a>
-        {/* <p>Bussiness</p> */}
         <a href="https://www.google.com/services">Bussiness</a>
-        {/* <p>How Search works</p> */}
         <a href="https://www.google.com/search/howsearchworks">How Search works</a>
        </div>
        <div className="flex justify-center space-x-8 md:ml-auto">
-        {/* <p>Privacy</p> */}
         <a href="https://policies.google.com/privacy">Privacy</a>
-        {/* <p>Terms</p> */}
         <a href="https://policies.google.com/terms">Terms</a>
         <p className="cursor-pointer">Settings</p>
        </div>
