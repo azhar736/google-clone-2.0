08b58a4094c3f86de92f7697c6551fea7908d568
Azhar Mehmood <azhark3113@gmail.com>
2022-09-14 16:44:31 +0500

    Complete-Clone

--- components/HeaderOptions.js
+++ components/HeaderOptions.js
@@ -5,9 +5,7 @@ function HeaderOptions() {
   return (
     <div className="flex w-full text-gray-700 justify-evenly
      text-sm lg:text-base lg:justify-start lg:space-x-36 lg:pl-52 border-b">
-     {/*Left*/}
      <div className="flex space-x-6">
-      
       <HeaderOption Icon={MagnifyingGlassIcon} title="All" selected />
       <HeaderOption Icon={PhotoIcon} title="Images" />
       <HeaderOption Icon={PlayIcon} title="Videos" />
@@ -15,7 +13,6 @@ function HeaderOptions() {
       <HeaderOption Icon={MapIcon} title="Maps" />
       <HeaderOption Icon={EllipsisVerticalIcon} title="More" />
      </div>
-     {/*Right*/}
      <div className="flex space-x-4">
         <p className="link font-semibold border-b-4 border-transparent hover:text-blue-500 cursor-pointer hover:border-blue-500">Settings</p>
         <p className="link font-semibold border-b-4 border-transparent hover:text-blue-500 cursor-pointer hover:border-blue-500">Tools</p>
